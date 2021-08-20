0b2ccd7e397e4de5c873c98b6ab6c22d9b0d0e6a
selinahussain <61423748+selinahussain@users.noreply.github.com>
2021-08-20 10:05:31 +0100

    tt

--- test/controllers/BasicControllerSpec.scala
+++ test/controllers/BasicControllerSpec.scala
@@ -2,18 +2,18 @@ package controllers
 
 import models.Vehicle
 import org.mockito.ArgumentMatchers._
-import org.mockito.Mockito.{when}
+import org.mockito.Mockito.when
 import org.scalatest.Matchers._
 import org.scalatestplus.mockito.MockitoSugar.mock
 import org.scalatestplus.play.PlaySpec
 import org.scalatestplus.play.guice.GuiceOneAppPerTest
 import play.api.Play.materializer
 import play.api.http.Status
-import play.api.libs.json.{Json}
-import play.api.mvc.{ControllerComponents}
+import play.api.libs.json.Json
+import play.api.mvc.ControllerComponents
+import play.api.test.Helpers._
 import play.api.test.{FakeRequest, Injecting}
 import repositories.DataRepository
-import play.api.test.Helpers._
 
 import scala.concurrent.{ExecutionContext, Future}
 
@@ -22,7 +22,6 @@ class BasicControllerSpec extends PlaySpec with GuiceOneAppPerTest with Injectin
   lazy val controllerComponents: ControllerComponents = app.injector.instanceOf[ControllerComponents]
   implicit lazy val executionContext: ExecutionContext = app.injector.instanceOf[ExecutionContext]
   val mockDataRepository: DataRepository = mock[DataRepository]
-
 
   object testController extends BasicController(
     controllerComponents,
@@ -47,7 +46,7 @@ class BasicControllerSpec extends PlaySpec with GuiceOneAppPerTest with Injectin
 
         val result = testController.getOneVehicle("BMW")(FakeRequest())
 
-       status(result) mustBe (Status.OK)
+        status(result) mustBe (Status.OK)
 
       }
 
@@ -91,4 +90,5 @@ class BasicControllerSpec extends PlaySpec with GuiceOneAppPerTest with Injectin
 
     }
 
-}}
+  }
+}
